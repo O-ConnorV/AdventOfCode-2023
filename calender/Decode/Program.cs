340e0237a5f9d241ac2cdd39eaa91d9ad832cb5a
O-ConnorV <147445642+O-ConnorV@users.noreply.github.com>
2023-12-03 00:27:27 +0100

    Delete calender directory

--- calender/Decode/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
-
-//int a1 = Decode.DecodeFolder.DecodeCalender("encrypted.txt");
-//Console.WriteLine(a1);
-
-int b1 = Decode.DecodeFolder.DecodeCalender2("encrypted.txt");
-Console.WriteLine(b1);
